4d9527179e90d98f30bc591eb28db2d3c18c22e4
Mohamed Habib Abid <118135433+mohamedhabibabid@users.noreply.github.com>
2025-08-01 04:29:56 +0800

    Update 1700. Number of Students Unable to Eat Lunch.cpp

--- DSA/QUEUE/1700. Number of Students Unable to Eat Lunch.cpp
+++ DSA/QUEUE/1700. Number of Students Unable to Eat Lunch.cpp
@@ -3,10 +3,7 @@ public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
        queue<int> q(students.begin(),students.end());
        stack<int>s(sandwiches.rbegin(), sandwiches.rend());
-       for(int i=0; i< students.size(); i++){
-            q.push(i);
-            s.push(i);
-       } 
+
        int stayed=0;
        while(!q.empty() && stayed<q.size()){
         int frq=q.front();
