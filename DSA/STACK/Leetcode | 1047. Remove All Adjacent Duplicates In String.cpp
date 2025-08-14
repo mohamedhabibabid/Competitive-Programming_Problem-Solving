3181c3e61b36ac5993b0f46776236650994da65f
Mohamed Habib Abid <118135433+mohamedhabibabid@users.noreply.github.com>
2025-08-14 16:24:27 +0800

    Create Leetcode | 1047. Remove All Adjacent Duplicates In String.cpp

---
+++ DSA/STACK/Leetcode | 1047. Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st; 
+        string ans=""; 
+        for(int i=0; i< s.size(); i++){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else if(st.top()==s[i]){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+
+        while(st.size()){
+            ans=st.top()+ans;
+            st.pop();
+        }
+
+        return ans;
+    }
+};
