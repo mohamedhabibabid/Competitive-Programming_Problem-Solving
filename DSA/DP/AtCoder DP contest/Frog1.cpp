2f6a504fed2438e8c2ebe828bbc4a9c8cfa2b9ee
Mohamed Habib Abid <118135433+mohamedhabibabid@users.noreply.github.com>
2025-12-27 21:07:49 +0800

    Update Frog1.cpp

--- DSA/DP/AtCoder DP contest/Frog1.cpp
+++ DSA/DP/AtCoder DP contest/Frog1.cpp
@@ -36,15 +36,6 @@ long long dp[N];
 //     dp[0]=0;
 //     dp[1]=abs(h[1]-h[0]);
 //     for(int i=2; i<=n; i++){
-//         dp[i]=INF;
-//         // if(min(h[i+1]-h[i],h[i+2]-h[i])==(h[i+1]-h[i])){
-//         //     dp[i]=h[i+1]-h[i];
-//         //     i++;
-//         // }else{
-//         //     i+=2;
-//         //     dp[i]=h[i+2]-h[i];
-
-//         // }
 //         dp[i]=min(
 //             dp[i-1]+abs(h[i]-h[i-1]),
 //             dp[i-2]+ abs(h[i]-h[i-2])
