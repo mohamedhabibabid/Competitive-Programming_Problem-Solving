90dde689856ff5d46a70299a06fdfc1bea85154d
Mohamed Habib Abid <118135433+mohamedhabibabid@users.noreply.github.com>
2026-01-29 11:42:14 +0800

    Create Links.cpp

---
+++ DSA/Links/Number Theory/Links.cpp
@@ -0,0 +1,8 @@
+Sieve : 
+https://cp-algorithms.com/algebra/sieve-of-eratosthenes.html#segmented-sieve
+
+Linear Sieve : (mazlt machftouch)
+https://cp-algorithms.com/algebra/prime-sieve-linear.html
+
+Primality test : (miller Rabin) 
+https://cp-algorithms.com/algebra/primality_tests.html
